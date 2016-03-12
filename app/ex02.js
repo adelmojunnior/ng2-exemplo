779d7ae382a8c2246d9ce17dce3366983e64a5b5
Adelmo Freitas <adelmo.freitas@serpro.gov.br>
2016-03-12 17:22:52 -0300

    Commit inicial

---
+++ app/ex02.js
@@ -0,0 +1,4 @@
+"use strict";
+var browser_1 = require('angular2/platform/browser');
+var application_1 = require('app/components/application/application');
+browser_1.bootstrap(application_1.default);
